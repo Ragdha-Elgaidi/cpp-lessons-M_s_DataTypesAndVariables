b42bc646435a8cde0a844801b63587e396935316
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-11-30 19:01:00 +0200

    Update SwapingThreeNumbers.cpp

--- SwapingThreeNumbers.cpp
+++ SwapingThreeNumbers.cpp
@@ -25,3 +25,23 @@ int main() {
 
 	return 0;
 }
+//Another solution
+#include<iostream>
+using namespace std;
+
+int main() {
+	int num1, num2, num3, temp = -1;
+
+	// Circulate them instead of direct 2 swaps
+	cin>>num1>>num2>>num3;
+	temp = num1;
+	num1 = num2;
+	num2 = num3;
+	num3 = temp;
+
+	cout<<num1<<" "<<num2<<" "<<num3<<"\n";
+
+
+	return 0;
+}
+
